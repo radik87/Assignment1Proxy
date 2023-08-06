6695f07f97cb15f8aa4f4049e71e6b70e6b812a1
Rodion.Skopych@e-consulting.com.ua <Rodion.Skopych@e-consulting.com.ua>
2023-08-06 14:49:06 +0300

    init

---
+++ Proxy/Controllers/ProxyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proxy.Services;
+
+namespace Proxy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProxyController : ControllerBase
+    {
+        private readonly GetReplacedTextService _getReplacedTextService;
+        public ProxyController(GetReplacedTextService getReplacedTextService)
+        {
+            _getReplacedTextService = getReplacedTextService;
+        }
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return await _getReplacedTextService.GetResponseFromUrl("https://localhost:7112/api/Service");
+        }
+    }
+}
---
+++ Proxy/Services/GetReplacedTextService.cs
@@ -0,0 +1,20 @@
+﻿namespace Proxy.Services
+{
+    public class GetReplacedTextService
+    {
+        public async Task<string> GetResponseFromUrl(string url)
+        {
+            string res;
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(url))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    res = apiResponse;
+                }
+            }
+            return res;
+        }
+    }
+}
